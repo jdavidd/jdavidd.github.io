90f30cbf597d1867633c1bcaad8f147e8b3921ab
Jitca David <j.davidd@yahoo.com>
2018-03-10 15:13:24 +0200

    modified for phones

--- javascript/index.js
+++ javascript/index.js
@@ -10,20 +10,20 @@ function click(){
 	   parent.removeChild(x);
 	}, true);
 }
-function allLetter(inputtxt){   
-          var letters = /^[A-Za-z]+$/;  
-          if(inputtxt.value.match(letters))  
-          {  
-           
-          return true;  
-          }  
-          else  
-          {  
-          
-          return false;  
-          } 
-         
-}  
+function allLetter(inputtxt){
+          var letters = /^[A-Za-z]+$/;
+          if(inputtxt.value.match(letters))
+          {
+
+          return true;
+          }
+          else
+          {
+
+          return false;
+          }
+
+}
 function verify_input_numbers(){
 	var first_name = document.querySelector(".verify_first_name");
 	var last_name = document.querySelector(".verify_last_name");
@@ -38,12 +38,12 @@ function verify_input_numbers(){
 	var phoneReg = /^\d{10}$/;
 	var zipReg = /^\d{8}$/;
 	var okk=0;
-	
-	
+
+
 
 		for( var i = 0 ;i < inp.length; i++)
 	{
-		
+
 		if(inp[i].checked)
 			if(inp[i].value==1)
 			{
@@ -63,11 +63,11 @@ function verify_input_numbers(){
 					{
 						if(email.value.match(emailReg))
 						{
-							
+
 							alert("All type of validation has done on OnSubmit event.");
 							return true;
 						} else
-						{	
+						{
 							alert("Email is not corect");
 							return false;
 						}
@@ -75,7 +75,7 @@ function verify_input_numbers(){
 					{
 						alert("zip is not corect");
 						return false;
-					}	
+					}
 				} else
 				{
 					alert("phone is not corect");
@@ -90,7 +90,7 @@ function verify_input_numbers(){
 			{
 				alert("First name is not valid ");
 				return false;
-			}  
+			}
 	}else
 	{
 		alert("Answer wrong, you're a robot!");
@@ -119,16 +119,37 @@ function generate_quiz(){
 			x[i].style.display = "none";
 
 		}
-		x[i].classList.remove("selected_field");                                    
+		x[i].classList.remove("selected_field");
 	}
 	x[y].style.display = "block";
 	x[y].classList.add("selected_field");
 
 }
+
+function myFunction() {
+	var x = window.matchMedia("(max-width: 700px)");
+	var image = document.getElementById("logo");
+    if (x.matches) { // If media query matches
+        image.src ="../Image-S/tesla.png";
+    }
+}
+function modify_navbar(){
+	var nav1 = document.getElementById("responsive1");
+	var nav2 = document.getElementById("responsive2");
+	var x = window.matchMedia("(max-width: 700px)");
+    if (x.matches) { // If media query matches
+      nav1.classList.remove("col-8");
+			nav1.classList.add("col-2");
+			nav2.classList.remove("col-4");
+			nav2.classList.add("col-10");
+		}
+}
+
 function start(){
 	click();
 	generate_quiz();
 	onSubmitt();
-
+	myFunction();
+	modify_navbar();
 }
 window.onload = start;
